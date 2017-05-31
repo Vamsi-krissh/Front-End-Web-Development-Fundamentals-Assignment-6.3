8aab4d0fe4ac3fe42e5245e8df1f9e71b7827604
Vamsi-krissh <vamsi1393@gmail.com>
2017-06-01 01:14:30 +0530

    6.3

---
+++ app1.js
@@ -0,0 +1,23 @@
+
+//IF loop
+
+var sumif = 0;    
+for(var i = 1; i <= 1000; i++)
+{
+    sumif += i;
+}
+console.log(sumif)
+
+
+
+//While Loop
+
+var i = 1;
+    var sumwhile = 0;
+    while (i <= 1000) 
+{
+        sumwhile = sumwhile + i;
+        ++i;        
+}
+console.log(sumwhile)  
+
